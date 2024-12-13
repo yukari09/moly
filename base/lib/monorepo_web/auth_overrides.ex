fb2d31ae81a0c35176219e7174eb2aa458174166
yukari <yukari@yukarideMacBook-Pro-2.local>
2024-12-13 15:42:44 +0800

    Info

--- base/lib/monorepo_web/auth_overrides.ex
+++ base/lib/monorepo_web/auth_overrides.ex
@@ -9,10 +9,10 @@ defmodule MonorepoWeb.AuthOverrides do
 
   # For a complete reference, see https://hexdocs.pm/ash_authentication_phoenix/ui-overrides.html
 
-  override AshAuthentication.Phoenix.Components.Banner do
-    set :image_url, "https://media.giphy.com/media/g7GKcSzwQfugw/giphy.gif"
-    set :text_class, "bg-red-500"
-  end
+  # override AshAuthentication.Phoenix.Components.Banner do
+  #   set :image_url, "https://media.giphy.com/media/g7GKcSzwQfugw/giphy.gif"
+  #   set :text_class, "bg-red-500"
+  # end
 
   # override AshAuthentication.Phoenix.Components.SignIn do
   #  set :show_banner false
