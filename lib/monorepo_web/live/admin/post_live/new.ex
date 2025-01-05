d7a5549dbb546166e4f8ef9e0f39213de630a080
Yukari <ssigong1@gmail.com>
2025-01-05 12:39:35 +0000

    sd

--- lib/monorepo_web/live/admin/post_live/new.ex
+++ lib/monorepo_web/live/admin/post_live/new.ex
@@ -8,8 +8,6 @@ defmodule MonorepoWeb.AdminPostLive.New do
     term_taxonomy_category =
       Monorepo.Terms.read_term_taxonomy!("category", nil, actor: socket.assigns.current_user)
       |> Ash.load!([:term], actor: socket.assigns.current_user)
-      |> build_category_tree()
-
 
 
     form = resource_to_form(Monorepo.Contents.Post, term_taxonomy_category)
@@ -108,7 +106,14 @@ defmodule MonorepoWeb.AdminPostLive.New do
   defp category_inputs(assigns) do
     ~H"""
     <.inputs_for :let={term_taxonomy} field={@form[:term_taxonomy]} skip_hidden={true}>
-      <.checkbox  field={term_taxonomy[:id]}  label={term_taxonomy[:term].value.name} />
+      <.checkbox field={term_taxonomy[:id]} label={term_taxonomy[:term].value.name} />
+      <%= if Enum.any?(term_taxonomy[:children]) do %>
+        <div class="ml-4">
+          <%= for child <- term_taxonomy[:children] do %>
+            <%= category_inputs(assigns |> Map.put(:form, child)) %>
+          <% end %>
+        </div>
+      <% end %>
     </.inputs_for>
     """
   end
