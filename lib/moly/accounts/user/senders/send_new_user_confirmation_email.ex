333ebd6201c308a77384fb55eb0fcf6b2114d5d8
yukari <ssigong1@gmail.com>
2025-04-08 14:46:39 +0800

    debug

--- lib/moly/accounts/user/senders/send_new_user_confirmation_email.ex
+++ lib/moly/accounts/user/senders/send_new_user_confirmation_email.ex
@@ -8,7 +8,7 @@ defmodule Moly.Accounts.User.Senders.SendNewUserConfirmationEmail do
 
   @impl true
   def send(user, token, _) do
-    if user.confirmed_at == nil do
+    if user.confirmed_at == nil || user.status == "inactive" do
       Moly.Accounts.Emails.deliver_email_confirmation_instructions(
         user,
         url(~p"/auth/user/confirm_new_user?#{[confirm: token]}")
