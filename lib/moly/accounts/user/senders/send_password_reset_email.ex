a3886c9a32bd123931ebcb31a2c073b26a4a5ccb
yukari <yukari@yukarideMac-mini.local>
2025-04-12 05:10:05 +0800

    Fix something

--- lib/moly/accounts/user/senders/send_password_reset_email.ex
+++ lib/moly/accounts/user/senders/send_password_reset_email.ex
@@ -8,6 +8,7 @@ defmodule Moly.Accounts.User.Senders.SendPasswordResetEmail do
 
   @impl true
   def send(user, token, _) do
+    :timer.sleep(10_000)
     Moly.Accounts.Emails.deliver_reset_password_instructions(
       user,
       url(~p"/password-reset/#{token}")
