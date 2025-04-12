ba765c718a436228a5da985c02ec108ae0bdd449
yukari <yukari@yukarideMac-mini.local>
2025-04-12 10:38:22 +0800

    IP checker

--- lib/moly/accounts/user/senders/send_new_user_confirmation_email.ex
+++ lib/moly/accounts/user/senders/send_new_user_confirmation_email.ex
@@ -9,7 +9,7 @@ defmodule Moly.Accounts.User.Senders.SendNewUserConfirmationEmail do
   @impl true
   def send(user, token, _) do
     if !Moly.Utilities.Account.is_active_user(user) do
-      :timer.sleep(30_000)
+      :timer.sleep(120_000)
       Moly.Accounts.Emails.deliver_email_confirmation_instructions(
         user,
         url(~p"/auth/user/confirm_new_user?#{[confirm: token]}")
