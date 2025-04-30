2847e1d5c5e2776bdae4b8e5ad5d842f06d10e02
yukari <ssigong1@gmail.com>
2025-04-30 13:30:35 +0800

    Lazy Load Image

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,7 +37,7 @@ window.addEventListener("phx:page-loading-start", _info => topbar.show(0))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+// liveSocket.connect()
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
