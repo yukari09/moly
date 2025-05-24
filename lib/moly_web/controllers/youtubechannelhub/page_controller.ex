e47aff91995d44191027418b0a0d39d97c771f10
yukari <ssigong1@gmail.com>
2025-05-25 06:24:32 +0800

    Add

--- lib/moly_web/controllers/youtubechannelhub/page_controller.ex
+++ lib/moly_web/controllers/youtubechannelhub/page_controller.ex
@@ -4,7 +4,7 @@ defmodule MolyWeb.YoutubeChannelHub.PageController do
   def index(conn, _params) do
     conn = put_layout(conn, false)
     page_title = "Youtube Thumbnail Viewer And Downloader"
-    page_description = "Extract high-quality thumbnails from any YouTube video in seconds. Professional tools for content creators, bloggers, and digital marketers. "
+    page_description = "Find youtube thumbnail high-quality thumbnails from any YouTube video in seconds. Professional tools for content creators, bloggers, and digital marketers. "
     render conn, :index, [page_title: page_title, page_description: page_description]
   end
 
